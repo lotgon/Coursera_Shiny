351a04a545d9ddd4d44dc5f0e7bd86c937b50c3b
lotgon <andrei1pazniak@gmail.com>
2016-03-24 18:40:41 +0200

    add algo

--- MartinGale/server.R
+++ MartinGale/server.R
@@ -6,44 +6,146 @@
 #
 
 library(shiny)
-# GetMaximumDropdown <- function (bars, tp )
-# {
-#     N<-nrow(bars)
-#     result<-as.data.table(data.frame(row.names=c("tp", "openTime", "tralOpen", "UpDown")))
-#     askFrom<-bars$askFrom
-#     bidFrom<-bars$bidFrom
-#     bidSpeed<-bars$bidSpeed
-#     askSpeed<-bars$askSpeed
-#     bidClose<-bars$bidClose
-#     askClose<-bars$askClose
-# }
+ GetMaximumDropdown <- function (bars, tp )
+ {
+    N<-nrow(bars)
+    result<-as.data.table(data.frame())
+    askFrom<-bars$askFrom
+    bidHigh<-bars$bidHigh
+    askLow<-bars$askLow
+    
+    openSell <- bidHigh[1]
+    maxPriceSell <-openSell
+    openTimeSell <- 1
+    
+    openBuy <- askLow[1]
+    minPriceBuy <-openBuy
+    openTimeBuy <- 1   
+    
+    for(i in 1:N)
+    {
+        if( askLow[i] > maxPriceSell ) #better to use askHigh if you need more precise model
+            maxPriceSell <- askLow[i]
+        if( askLow[i] < maxPriceSell - tp) #not equal because we want to imitate not ideal execution 
+        {
+            result<-rbindlist(list(result, data.table(tp, 
+                                                      side="sell",
+                                                      openTimeIndex=openTimeSell,
+                                                      drawdown=maxPriceSell - openSell,
+                                                      age=i-openTimeSell,
+                                                      openPrice=openSell,
+                                                      openTime=askFrom[openTimeSell],
+                                                      closeTime=askFrom[i])))
+            if( i!= N)
+            {
+                openSell <- bidHigh[i+1] #open new order on next step... better to use bidClose instead of high
+                maxPriceSell <- openSell
+                openTimeSell <- i+1
+            }
+        }
+        #the same logic for buy order
+        if( bidHigh[i] < minPriceBuy) 
+            minPriceBuy <- bidHigh[i]
+        if( bidHigh[i] > minPriceBuy + tp ) #not equal because we want to imitate not ideal execution 
+        {
+            result<-rbindlist(list(result, data.table(tp, 
+                                                      side="buy",
+                                                      openTimeIndex=openTimeBuy,
+                                                      drawdown=openBuy - minPriceBuy,
+                                                      age=i-openTimeBuy,
+                                                      openPrice=openBuy,
+                                                      openTime=askFrom[openTimeBuy],
+                                                      closeTime=askFrom[i])))
+            if( i!= N)
+            {
+                openBuy <- askLow[i+1] #open new order on next step... better to use bidClose instead of high
+                minPriceBuy <- openBuy
+                openTimeBuy <- i+1
+            }
+        }
+        
+     }
+    result<-rbindlist(list(result, data.table(tp, 
+                                              "sell",
+                                              openTimeSell,
+                                              maxPriceSell - openSell,
+                                              NA,
+                                              openSell,
+                                              askFrom[openTimeSell],
+                                              askFrom[i])))
+    
+    result<-rbindlist(list(result, data.table(tp, 
+                                              "buy",
+                                              openTimeBuy,
+                                              openBuy - minPriceBuy,
+                                              NA,
+                                              openBuy,
+                                              askFrom[openTimeBuy],
+                                              askFrom[i])))
+    
+    result
+ }
 
 shinyServer(function(input, output) {
 
     library(lubridate) 
     library(ggplot2)
     library(data.table)
-
+    library(downloader)
+    volumeLots<<-0.01
+    
     symbolInput<- reactive({
+        withProgress(message = 'Downloading data', value = 0, {
+            
         symbolName<<-input$symbolInput
-        sourceLink<-sprintf("https://github.com/lotgon/Coursera_Shiny/blob/master/MartinGale/data/%s.zip?raw=true", symbolName)
-        file <- paste(tempdir(), symbolName, sep =  "")
+        sourceLink<-sprintf("https://github.com/lotgon/Coursera_Shiny/blob/master/PrepareData/data/%s.zip?raw=true", symbolName)
+        file <- paste(tempdir(), symbolName, sep =  "\\")
         if(!file.exists(file))
-            download.file(sourceLink, file, mode="wb") 
+            download(sourceLink, file, mode="wb") 
+        incProgress(0.5)
         
-        d<-fread(unzip(file))
-        d[,askFrom:=ymd_hms(askFromString, tz="GMT")]
-        d[,askFromString:=NULL]  
+         d<-fread(unzip(file))
+         d[,askFrom:=ymd_hms(askFromString, tz="GMT")]
+         d[,askFromString:=NULL]  
+         d<-d[.N:1]
+         incProgress(0.75)
+        })
         d
+    })
+    calcInput<- reactive({
+        data <- symbolInput()      
+        r1<- GetMaximumDropdown(data, input$tp)
+        r1[,DrawdownInTp:=drawdown/tp]
+        r1[,index:=1:.N]
+        r1[,usedMargin:=tp*volumeLots*10*2^(DrawdownInTp)]
+        r1[,balanceGrowth:=tp*volumeLots*10*index]
+        r1
     })
 
   output$BidChart <- renderPlot({
     data <- symbolInput()      
-    ggplot(data, aes(x=data$askFrom, y=data$bidHigh)) + geom_line() + ggtitle(symbolName) + xlab("Date") + ylab("Price")
+    ggplot(data, aes(x=data$askFrom, y=data$bidHigh*10^-5)) + geom_line() + ggtitle(symbolName) + xlab("Date") + ylab("Price")
   })
-  
-#   output$tradeReportsTable <-renderTable({
-#       data <- symbolInput()      
-#   })
+  output$MarginChart <- renderPlot({
+      r1 <- calcInput()      
+      ggplot(r1, aes(x=openTime, y=usedMargin)) + geom_line() + ylab("Required deposit, USD") + ggtitle("Margin requirement for account")
+  })
+  output$ProfitChart <- renderPlot({
+      r1 <- calcInput()      
+      ggplot(r1, aes(x=openTime, y=balanceGrowth+tp)) + geom_line() + ylab("Account Balance, USD") + ggtitle("Account Profit")
+  })
+  output$percentageIncome <- renderPrint({
+      r1 <- calcInput()      
+      round(input$tp*volumeLots*10*nrow(r1) *100 / max(r1$usedMargin))
+      
+  })
+  output$ordersNumber <- renderPrint({
+      r1 <- calcInput()      
+      nrow(r1)
+  })
+#    output$tradeReportsTable <-renderTable({
+#        data <- symbolInput()      
+#        r[, .(openTime, closeTime, side, usedMargin, balanceGrowth)]
+#    })
 
 })
--- MartinGale/ui.R
+++ MartinGale/ui.R
@@ -15,12 +15,20 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-        selectInput("symbolInput", "Choose a symbol:", choices = c("EURUSD", "XAUUSD", "XAGUSD"))
+        selectInput("symbolInput", "Choose a symbol:", choices = c("EURUSD", "AUDUSD", "GBPUSD", "LTCUSD", "NZDUSD"))
+        ,   sliderInput("tp", "Take Profit:", min = 100,  max = 1000, value = 500, step=50)
         ),
 
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("BidChart")
+      plotOutput("BidChart"),
+      plotOutput("MarginChart"),
+      plotOutput("ProfitChart"),
+      helpText('Income in % per period:'),
+      verbatimTextOutput('percentageIncome'),
+      helpText('Orders number:'),
+      verbatimTextOutput('ordersNumber')
+      
 #       ,h4("Estimated Trade History"),
 #       ,tableOutput("tradeReportsTable")
     )
